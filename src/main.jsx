0eac480db8e415510b3934373044c59183e1870b
Gaurav Sharma <gauravsharma16072001@gmail.com>
2025-03-29 22:31:27 +0530

    added helmet

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import './style.css'
 import { BrowserRouter } from "react-router-dom";
 import { ContextProvider } from './firebase/FirebaseContext.jsx';
 import { AppwriteContextProvider } from './appwrite/AppwriteContext.jsx';
-
+import { HelmetProvider } from 'react-helmet-async';
 ReactDOM.createRoot(document.getElementById('root')).render(
     <ContextProvider>
       <AppwriteContextProvider>
       <BrowserRouter>
+      <HelmetProvider>
         <App />
+      </HelmetProvider>
       </BrowserRouter>
       </AppwriteContextProvider>
     </ContextProvider>
